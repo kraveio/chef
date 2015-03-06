c8622ec46f84b22c99adcb0cb3fbce7110d164e2
Franklin Wise <franklin@krave.io>
2015-03-05 20:57:12 -0800

    removed aws depend

--- consul-kio/recipes/dependencies.rb
+++ consul-kio/recipes/dependencies.rb
@@ -2,8 +2,6 @@
 # recipe::dependency
 #
 
-include_recipe 'aws-util::opsworks_hosts'
-
 layer_name = node[:consul_kio][:layers][:consul]
 node.normal['consul']['servers'] = node[:stack][layer_name][:ips]
 
