27b2a68543949ca6c58aca2ed776c000e809a8a9
Franklin Wise <franklin@krave.io>
2015-04-15 15:07:26 -0700

    partial refactor

--- up/all.tf
+++ up/all.tf
@@ -1,5 +1,5 @@
-module "bootstrap" {
-    source = "./bootstrap"
+module "vpc" {
+    source = "./vpc"
 
 	## Common ##
 
@@ -22,34 +22,34 @@ module "bootstrap" {
 }
 
 output "vpc_id" {
-    value = "${module.bootstrap.vpc_id}"
+    value = "${module.vpc.vpc_id}"
 }
 
 output "jump_ip" {
-	value = "${module.bootstrap.jump_ip}"
+	value = "${module.vpc.jump_ip}"
 }
 
-module "consul" {
-    source = "./consul"
-
-	## Common ##
-
-	access_key = "${var.access_key}"
-	secret_key = "${var.secret_key}"
-	aws_account_id = "${var.aws_account_id}"
-	key_name = "${var.key_name}"
-
-	## Region ##
-
-	region = "${var.region}"
-	zone_default = "${var.zone_default}"
-	zone_alt = "${var.zone_alt}"
-
-	## Specific ##
-
-	ami =  "${lookup(var.amis, var.region)}"
-	vpc_id = "${module.bootstrap.vpc_id}"
-	sg_ssh_base_id = "${module.bootstrap.sg_ssh_base_id}"
-	jump_ip = "${module.bootstrap.jump_ip}"
-}
+#module "consul" {
+#    source = "./consul"
+#
+#	## Common ##
+#
+#	access_key = "${var.access_key}"
+#	secret_key = "${var.secret_key}"
+#	aws_account_id = "${var.aws_account_id}"
+#	key_name = "${var.key_name}"
+#
+#	## Region ##
+#
+#	region = "${var.region}"
+#	zone_default = "${var.zone_default}"
+#	zone_alt = "${var.zone_alt}"
+#
+#	## Specific ##
+#
+#	ami =  "${lookup(var.amis, var.region)}"
+#	vpc_id = "${module.bootstrap.vpc_id}"
+#	sg_ssh_base_id = "${module.bootstrap.sg_ssh_base_id}"
+#	jump_ip = "${module.bootstrap.jump_ip}"
+#}
 
--- up/bootstrap/s3.tf
+++
@@ -1,7 +0,0 @@
-
-
-# TODO: copy files to S3
-#resource "aws_s3_bucket" "bootstrap" {
-#    bucket = "bootstrap.${var.domain}"
-#    acl = "private"
-#}
